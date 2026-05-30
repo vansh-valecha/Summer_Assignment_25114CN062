7415f2df1a9b169c0d4c8d737f961a3c4670a578
vansh-valecha <valechavansh560@gmail.com>
2026-05-30 15:22:04 +0530

    Add LCM calculation program in C

---
+++ Day3/4LCM.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, max, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    max = (a > b) ? a : b;
+
+    while (1) {
+        if (max % a == 0 && max % b == 0) {
+            lcm = max;
+            break;
+        }
+        max++;
+    }
+
+    printf("LCM of %d and %d is %d\n", a, b, lcm);
+
+    return 0;
+}
