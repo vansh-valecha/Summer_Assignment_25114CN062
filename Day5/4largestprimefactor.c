51dada9560750632c5bd3d585d2d90db77955c40
vansh-valecha <valechavansh560@gmail.com>
2026-06-01 02:25:11 +0530

    Create 4largestprimefactor.c

---
+++ Day5/4largestprimefactor.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int prime(int n)
+{
+    int i, isprime = 0;
+    for (i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            isprime = 1;
+            break;
+        }
+    }
+
+    if (isprime == 0)
+    {
+        return n;
+    }
+}
+
+int main()
+{
+    int n, i, larg = 0;
+
+    printf("enter a no:");
+    scanf("%d", &n);
+
+    if (n > 0)
+    {
+        for (i = 2; i <= n; i++)
+        {
+            if (n % i == 0 && i == prime(i))
+            {
+                larg = i;
+            }
+        }
+        printf("largest prime factor of %d is %d", n, larg);
+    }
+
+    else
+    {
+        printf("u entered a wrong no");
+    }
+    return 0;
+}
