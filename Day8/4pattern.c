d0d117ec3a38e129c3a404b1f5b6d765192ef46c
vansh-valecha <valechavansh560@gmail.com>
2026-06-04 01:16:43 +0530

    Create 4pattern.c

---
+++ Day8/4pattern.c
@@ -0,0 +1,21 @@
+/*
+1
+22
+333
+4444*/
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("enter no of rows:");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
