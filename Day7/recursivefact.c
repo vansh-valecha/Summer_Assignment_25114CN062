d35f685ac2e850548be47d2abacc12e89413ae70
vansh-valecha <valechavansh560@gmail.com>
2026-06-03 14:38:37 +0530

    Add recursive factorial function in C

---
+++ Day7/recursivefact.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+    else if (n == 1)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+int main()
+{
+    int n;
+    printf("enter a no:");
+    scanf("%d", &n);
+    printf("fact of %d is %d", n, fact(n));
+}
