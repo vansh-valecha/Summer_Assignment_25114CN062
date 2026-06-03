5c556442e62168640f7bdc9b43d97c9f4b2b8f0f
vansh-valecha <valechavansh560@gmail.com>
2026-06-03 14:41:14 +0530

    Create 2recursivefibo.c

---
+++ Day7/2recursivefibo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fibo(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    else
+        return fibo(n - 1) + fibo(n - 2);
+}
+int main()
+{
+    int n, i;
+    printf("enter term upto which u want to print:");
+    scanf("%d", &n);
+    for (i = 0; i <= n - 1; i++)
+    {
+        printf("%d ", fibo(i));
+    }
+}
